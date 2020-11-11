dc40133d0b722c373f44e1c7233a27f36c0bdf5c
amanzou anas <amenzouanas39@gmail.com>
2020-11-11 21:10:10 +0100

    adding a profile page

--- src/Pages/AuthenticatedPages/Profile.js
+++ src/Pages/AuthenticatedPages/Profile.js
@@ -5,7 +5,9 @@ import {
   CardMedia,
   Divider,
   Grid,
+  IconButton,
   ListItem,
+  ListItemAvatar,
   Paper,
   Typography,
 } from "@material-ui/core";
@@ -19,7 +21,8 @@ import PermContactCalendarIcon from "@material-ui/icons/PermContactCalendar";
 import React from "react";
 import { UserContext, UserDispatchContext } from "../../Context/UserContext";
 import WithAuthentication from "./WithAuthentication";
-import image from "../../assets/placeholder.png";
+import image from "../../assets/placeholder.jpg";
+import { PhotoCamera } from "@material-ui/icons";
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -37,13 +40,16 @@ const useStyles = makeStyles((theme) => ({
     height: "250px",
     width: "250px",
   },
+  title: {
+    fontWeight: 600,
+  },
 }));
 const InfosIcons = {
-  nom: <InfoIcon />,
-  prenom: <InfoIcon />,
-  adresseMail: <ContactMailIcon />,
-  numTele: <ContactPhoneIcon />,
-  login: <PermContactCalendarIcon />,
+  nom: <InfoIcon color="secondary" />,
+  prenom: <InfoIcon color="secondary" />,
+  adresseMail: <ContactMailIcon color="secondary" />,
+  numTele: <ContactPhoneIcon color="secondary" />,
+  login: <PermContactCalendarIcon color="secondary" />,
 };
 
 const infosTitle = {
@@ -79,18 +85,35 @@ function Profile() {
                   image={image}
                   // title="Contemplative Reptile"
                 />
+                <input
+                  accept="image/*"
+                  hidden
+                  className={classes.input}
+                  id="icon-button-file"
+                  type="file"
+                />
+                <label htmlFor="icon-button-file">
+                  <IconButton
+                    color="primary"
+                    aria-label="upload picture"
+                    component="span"
+                  >
+                    <PhotoCamera />
+                  </IconButton>
+                </label>
                 <Divider />
               </Grid>
-              {/* <Grid item xs={2}>
-            <Avatar>I</Avatar>
-          </Grid> */}
               {title.map((value) => (
                 <>
                   {console.log(infosTitle[value])}
                   <Grid item xs={6}>
                     <ListItem>
-                      {InfosIcons[value]}
-                      <Typography variant="h6" component="h2">
+                      <ListItemAvatar>{InfosIcons[value]}</ListItemAvatar>
+                      <Typography
+                        className={classes.title}
+                        variant="h6"
+                        component="h2"
+                      >
                         {infosTitle[value]}:
                       </Typography>
                     </ListItem>
